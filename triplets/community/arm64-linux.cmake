5f2535a49ecc9cc8c48d47aa3b10ba7690442bcf
Chris <chris@monoclesecurity.com>
2023-02-28 12:28:49 +0000

    Fixing arm64 linux triplet

--- triplets/community/arm64-linux.cmake
+++ triplets/community/arm64-linux.cmake
@@ -1,5 +1,13 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+
+if ((${PORT} MATCHES ".*qt5.*") OR (${PORT} MATCHES "openssl"))
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_FIXUP_ELF_RPATH ON)
